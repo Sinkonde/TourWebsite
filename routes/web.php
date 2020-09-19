87e29ef8e6e4c9a5b0d2741f886b30db361606c5
Sinkonde <raphaelsinkonde1991@gmail.com>
2020-09-19 22:01:19 +0300

    So far

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,4 @@ Route::get('/home', "WebsiteController@home")->name('site.home');
 
 Auth::routes();
 
-//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
